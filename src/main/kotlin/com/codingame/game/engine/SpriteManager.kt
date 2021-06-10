067a28b43316e1c37fe017e6430e315e93deab6c
Congr00 <lukasklasinski@gmail.com>
2021-06-10 19:45:51 +0200

    Group numbers are now dynamic and changes/moves with objects. Fixed scaling of group numbers

--- src/main/kotlin/com/codingame/game/engine/SpriteManager.kt
+++ src/main/kotlin/com/codingame/game/engine/SpriteManager.kt
@@ -1,5 +1,9 @@
 package com.codingame.game.engine
 
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.Some
+import com.codingame.gameengine.module.entities.BitmapText
 import com.codingame.gameengine.module.entities.Curve
 import com.codingame.gameengine.module.entities.GraphicEntityModule
 import com.codingame.gameengine.module.entities.Sprite
@@ -14,9 +18,9 @@ class SpriteManager(
 ) {
     private var visionBlocks: Array<Sprite> = emptyArray()
 
-    private class SpritePair(val sprite: Sprite, var id: Int)
+    private class SpriteData(val sprite: Sprite, var id: Int, var text: Option<BitmapText> = None)
 
-    private var spriteContainer: HashMap<String, ArrayList<SpritePair>> = HashMap()
+    private var spriteContainer: HashMap<String, ArrayList<SpriteData>> = HashMap()
 
     init {
         entities.forEach { el ->
@@ -27,37 +31,6 @@ class SpriteManager(
                         spriteContainer[textureLoc] = arrayListOf()
                     }
                     newSprite(textureLoc, e)
-                }
-
-                if (e.isInteractable){
-                    graphicEntityModule.createBitmapText().apply {
-                        when (e.interactionTarget) {
-                            is ActionTarget.Group -> {
-                                text = (e.interactionTarget as ActionTarget.Group).gid.toString()
-                            }
-                            is ActionTarget.Template -> {
-                                text = (e.interactionTarget as ActionTarget.Template).tid.toString()
-                            }
-                        }
-                        font = "Pixeled"
-                        fontSize = 8
-                        x = ((e.position.x + 1) * scale * 32 - 18 * scale).toInt()
-                        y = ((e.position.y + 1) * scale * 32 - 12 * scale).toInt()
-                        zIndex = 2
-                        tint = 0xf93130
-                    }
-                }
-                else if (e.hasTemplate){
-                    if (e.tid > 1) {
-                        graphicEntityModule.createBitmapText().apply {
-                            text = e.tid.toString()
-                            font = "Pixeled"
-                            fontSize = 5
-                            x = ((e.position.x + 1) * scale * 32 - 7 * scale).toInt()
-                            y = ((e.position.y + 1) * scale * 32 - 7 * scale).toInt()
-                            zIndex = 2
-                        }
-                    }
                 }
             }
 
@@ -91,25 +64,62 @@ class SpriteManager(
         return textureLoc
     }
 
+    private fun newSpriteIndicatorText(entity: AnyGameEntity): Option<BitmapText> {
+        if (entity.isInteractable) {
+            return Some(graphicEntityModule.createBitmapText().apply {
+                when (entity.interactionTarget) {
+                    is ActionTarget.Group -> {
+                        text = (entity.interactionTarget as ActionTarget.Group).gid.toString()
+                    }
+                    is ActionTarget.Template -> {
+                        if ((entity.interactionTarget as ActionTarget.Template).tid < 2) {
+                            return None
+                        }
+                        text = (entity.interactionTarget as ActionTarget.Template).tid.toString()
+                    }
+                }
+                font = "Pixeled"
+                fontSize = (8 * scale / 2).toInt()
+                x = ((entity.position.x + 1) * scale * 32 - 18 * scale).toInt()
+                y = ((entity.position.y + 1) * scale * 32 - 12 * scale).toInt()
+                zIndex = 2
+                tint = 0xf93130
+            })
+        } else if (entity.hasTemplate) {
+            if (entity.tid > 1) {
+                return Some(graphicEntityModule.createBitmapText().apply {
+                    text = entity.tid.toString()
+                    font = "Pixeled"
+                    fontSize = (5 * scale / 1.5).toInt()
+                    x = ((entity.position.x + 1) * scale * 32 - 7 * scale).toInt()
+                    y = ((entity.position.y + 1) * scale * 32 - 8 * scale).toInt()
+                    zIndex = 5
+                    tint = 0xffffff
+                })
+            }
+        }
+        return None
+    }
+
     private fun newSprite(hash: String, entity: AnyGameEntity) {
-        spriteContainer[hash]?.add(SpritePair(graphicEntityModule.createSprite().apply {
+        spriteContainer[hash]?.add(SpriteData(graphicEntityModule.createSprite().apply {
             image = hash
             setX((entity.position.x * 32 * scale).toInt(), Curve.NONE)
             setY((entity.position.y * 32 * scale).toInt(), Curve.NONE)
             setScale(scale)
             zIndex = entity.texture.zIndex
-        }, entity.spriteID))
+        }, entity.spriteID, newSpriteIndicatorText(entity)))
     }
 
-    fun swapButton(btn: AnyGameEntity){
-        if (btn.hasTexture){
-            when (btn.texture.filepath){
+    fun swapButton(btn: AnyGameEntity) {
+        if (btn.hasTexture) {
+            when (btn.texture.filepath) {
                 Textures.BUTTON_ON.filepath -> {
                     freeSprite(btn)
                     btn.texture = Textures.BUTTON_OFF
                     allocateSprite(btn, btn.position)
                 }
-                Textures.BUTTON_OFF.filepath ->{
+                Textures.BUTTON_OFF.filepath -> {
                     freeSprite(btn)
                     btn.texture = Textures.BUTTON_ON
                     allocateSprite(btn, btn.position)
@@ -129,6 +139,19 @@ class SpriteManager(
                 if (p.id == entity.spriteID && p.sprite.isVisible) {
                     p.sprite.x = (newPosition.x * 32 * scale).toInt()
                     p.sprite.y = (newPosition.y * 32 * scale).toInt()
+                    if (p.text is Some) {
+                        if (entity.isInteractable) {
+                            (p.text as Some<BitmapText>).value.x =
+                                ((entity.position.x + 1) * scale * 32 - 18 * scale).toInt()
+                            (p.text as Some<BitmapText>).value.y =
+                                ((entity.position.y + 1) * scale * 32 - 12 * scale).toInt()
+                        } else {
+                            (p.text as Some<BitmapText>).value.x =
+                                ((entity.position.x + 1) * scale * 32 - 7 * scale).toInt()
+                            (p.text as Some<BitmapText>).value.y =
+                                ((entity.position.y + 1) * scale * 32 - 7 * scale).toInt()
+                        }
+                    }
                     break
                 }
             }
@@ -141,6 +164,9 @@ class SpriteManager(
             for (p in spriteContainer[textureLoc]!!) {
                 if (p.id == entity.spriteID) {
                     p.sprite.isVisible = false
+                    if (p.text is Some) {
+                        (p.text as Some<BitmapText>).value.isVisible = false
+                    }
                     break
                 }
             }
@@ -162,6 +188,48 @@ class SpriteManager(
                         p.id = entity.spriteID
                         p.sprite.isVisible = true
                         allocated = true
+                        if (p.text is None) {
+                            p.text = newSpriteIndicatorText(entity)
+                        }
+                        if (entity.isInteractable) {
+                            val text = when (entity.interactionTarget) {
+                                is ActionTarget.Group -> {
+                                    (entity.interactionTarget as ActionTarget.Group).gid.toString()
+                                }
+                                is ActionTarget.Template -> {
+                                    if ((entity.interactionTarget as ActionTarget.Template).tid < 2) {
+                                        break
+                                    } else {
+                                        (entity.interactionTarget as ActionTarget.Template).tid.toString()
+                                    }
+                                }
+                                else -> {
+                                    break
+                                }
+                            }
+                            (p.text as Some<BitmapText>).value.text = text
+                            (p.text as Some<BitmapText>).value.x =
+                                ((entity.position.x + 1) * scale * 32 - 18 * scale).toInt()
+                            (p.text as Some<BitmapText>).value.y =
+                                ((entity.position.y + 1) * scale * 32 - 12 * scale).toInt()
+                            (p.text as Some<BitmapText>).value.isVisible = true
+                            (p.text as Some<BitmapText>).value.tint = 0xf93130
+                            (p.text as Some<BitmapText>).value.fontSize = (8 * scale / 2).toInt()
+                            (p.text as Some<BitmapText>).value.zIndex = 2
+                        } else if (entity.hasTemplate) {
+                            if (entity.tid < 2) {
+                                break
+                            }
+                            (p.text as Some<BitmapText>).value.text = entity.tid.toString()
+                            (p.text as Some<BitmapText>).value.x =
+                                ((entity.position.x + 1) * scale * 32 - 7 * scale).toInt()
+                            (p.text as Some<BitmapText>).value.y =
+                                ((entity.position.y + 1) * scale * 32 - 8 * scale).toInt()
+                            (p.text as Some<BitmapText>).value.isVisible = true
+                            (p.text as Some<BitmapText>).value.tint = 0xffffff
+                            (p.text as Some<BitmapText>).value.fontSize = (5 * scale / 1.5).toInt()
+                            (p.text as Some<BitmapText>).value.zIndex = 5
+                        }
                         break
                     }
                 }
@@ -179,6 +247,9 @@ class SpriteManager(
         for (list in spriteContainer.values) {
             for (p in list) {
                 p.sprite.isVisible = false
+                if (p.text is Some) {
+                    (p.text as Some<BitmapText>).value.isVisible = false
+                }
             }
         }
         entities.forEach { el ->
